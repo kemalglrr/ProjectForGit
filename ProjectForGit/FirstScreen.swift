140b273f86ad2e948606a6f388b1b60d440f5b9f
Kemal <kemalglrr@gmail.com>
2023-08-26 15:57:10 +0300

    Projenin başlangıcı yapıldı.

---
+++ ProjectForGit/FirstScreen.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  ProjectForGit
+//
+//  Created by Kemal Güler on 26.08.2023.
+//
+
+import UIKit
+
+class FirstScreen: UIViewController {
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let button: UIButton = {
+        let button = UIButton()
+        button.clipsToBounds = true
+        button.isEnabled = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        configureContent()
+    }
+}
+
+// MARK: - UILayout
+extension FirstScreen {
+    
+    private func addSubviews() {
+        addLabel()
+        addButton()
+    }
+    
+    private func addLabel() {
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 256),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)
+        ])
+    }
+    
+    private func addButton() {
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 50),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.widthAnchor.constraint(equalToConstant: 100),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
+
+// MARK: - Configure
+extension FirstScreen {
+    
+    private func configureContent() {
+        configureView()
+        configureLabel()
+        configureButton()
+        configureTargets()
+    }
+    
+    private func configureView() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    private func configureLabel() {
+        label.font = UIFont(name: "TrebuchetMS", size: 18)
+        label.text = "Butona Basın Ve Bir Sonraki Ekrana İlerleyin."
+        label.textColor = .cyan
+    }
+    
+    private func configureButton() {
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .black
+        button.setTitle("İleri", for: .normal)
+        button.titleLabel?.font = UIFont(name: "TrebuchetMS-Bold", size: 20)
+        button.tintColor = .white
+    }
+    
+    private func configureTargets() {
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+}
+
+// MARK: - Actions
+extension FirstScreen {
+    
+    @objc
+    private func buttonTapped() {
+        let vc = SecondScreen()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
--- ProjectForGit/SecondScreen.swift
+++ ProjectForGit/SecondScreen.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondScreen.swift
 //  ProjectForGit
 //
 //  Created by Kemal Güler on 26.08.2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondScreen: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
     }
 
-
 }
-
