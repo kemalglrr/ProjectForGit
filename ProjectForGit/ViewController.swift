31531b21b0b4fd60562797f2bca7c5fa2dfba21c
Kemal <kemalglrr@gmail.com>
2023-08-26 15:39:31 +0300

    Initial Commit

---
+++ ProjectForGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectForGit
+//
+//  Created by Kemal Güler on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
