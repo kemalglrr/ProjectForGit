d4cbbafcfa3b57c298b3a37f8fc12869e80667aa
Kemal <kemalglrr@gmail.com>
2023-08-27 20:21:30 +0300

    projede ikinci ekrana dönüş için buton ve event'i eklendi.

--- ProjectForGit/SecondScreen.swift
+++ ProjectForGit/SecondScreen.swift
@@ -16,11 +16,19 @@ class SecondScreen: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.clipsToBounds = true
+        button.isEnabled = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubViews()
-        configureContent()
+        configureContents()
     }
 }
 
@@ -29,14 +37,25 @@ extension SecondScreen {
     
     private func addSubViews() {
         addLabel()
+        addButton()
     }
     
     private func addLabel() {
         view.addSubview(label)
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 256),
             label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
             label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8)
+        ])
+    }
+    
+    private func addButton() {
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 50),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.widthAnchor.constraint(equalToConstant: 100),
+            button.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
@@ -44,9 +63,11 @@ extension SecondScreen {
 // MARK: - Configure
 extension SecondScreen {
     
-    private func configureContent() {
+    private func configureContents() {
         configureView()
         configureLabel()
+        configureButton()
+        configureTargets()
     }
     
     private func configureView() {
@@ -56,6 +77,27 @@ extension SecondScreen {
     private func configureLabel() {
         label.font = UIFont(name: "TrebuchetMS", size: 18)
         label.textColor = .white
-        label.text = "Geri Dönmek İçin Ekranın Sol Sınırından Sağa Doğru Kaydırınız."
+        label.text = "Geri Dönmek İçin Ekranın Sol Sınırından Sağa Doğru Kaydırın Veya Butona Basınız."
+    }
+    
+    private func configureButton() {
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .lightGray
+        button.setTitle("Geri", for: .normal)
+        button.titleLabel?.font = UIFont(name: "TrebuchetMS-Bold", size: 20)
+        button.tintColor = .black
+    }
+    
+    private func configureTargets() {
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+}
+
+// MARK: - Actions
+extension SecondScreen {
+    
+    @objc
+    private func buttonTapped() {
+        navigationController?.popToRootViewController(animated: true)
     }
 }
